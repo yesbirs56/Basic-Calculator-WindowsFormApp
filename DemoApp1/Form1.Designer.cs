27852d44b6bcdf98a1fb495b61083df5636fbbc9
Yesbir Singh <YesbirS@bhavnacorp.com>
2021-05-27 15:16:09 +0530

    BasicCalc1

---
+++ DemoApp1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace DemoApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDesignation = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(154, 147);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Number 1 :";
+            this.lblName.Click += new System.EventHandler(this.lblName_Click);
+            // 
+            // lblDesignation
+            // 
+            this.lblDesignation.AutoSize = true;
+            this.lblDesignation.Location = new System.Drawing.Point(154, 197);
+            this.lblDesignation.Name = "lblDesignation";
+            this.lblDesignation.Size = new System.Drawing.Size(59, 13);
+            this.lblDesignation.TabIndex = 1;
+            this.lblDesignation.Text = "Number 2 :";
+            this.lblDesignation.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(229, 144);
+            this.txtNumber1.Multiline = true;
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(200, 25);
+            this.txtNumber1.TabIndex = 3;
+            this.txtNumber1.TextChanged += new System.EventHandler(this.txtNumber1_TextChanged);
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(229, 194);
+            this.txtNumber2.Multiline = true;
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(200, 23);
+            this.txtNumber2.TabIndex = 4;
+            this.txtNumber2.TextChanged += new System.EventHandler(this.txtNumber2_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(519, 138);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(56, 31);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(642, 138);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(56, 31);
+            this.btnSubtract.TabIndex = 7;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(642, 188);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(56, 31);
+            this.btnDivide.TabIndex = 8;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(519, 188);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(56, 31);
+            this.btnMultiply.TabIndex = 9;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(381, 262);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 469);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.lblDesignation);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDesignation;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ DemoApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DemoApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNumber1.Text);
+            int num2 = int.Parse(txtNumber2.Text);
+            lblResult.Text = num1 - num2 + "";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNumber1.Text);
+            int num2 = int.Parse(txtNumber2.Text);
+            lblResult.Text = num1 + num2 + "";
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNumber1.Text);
+            int num2 = int.Parse(txtNumber2.Text);
+            lblResult.Text = num1 * num2 + "";
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNumber1.Text);
+            int num2 = int.Parse(txtNumber2.Text);
+            if (num2 == 0)
+            {
+                lblResult.Text = "Inf";
+                return;
+            }
+            lblResult.Text = num1 / num2 + "";
+        }
+        private string oldValue1 = "";
+        private void txtNumber1_TextChanged(object sender, EventArgs e)
+        {
+            string num = txtNumber1.Text;
+
+            bool isValidInt = Int32.TryParse(num, out int number1);
+            if (txtNumber1.Text == "")
+            {
+                oldValue1 = num;
+            }
+            else if (!isValidInt)
+            {
+                txtNumber1.Text = oldValue1;
+            }
+            else
+            {
+                oldValue1 = txtNumber1.Text;
+            }
+        }
+
+        private string oldValue2 = "";
+
+        private void txtNumber2_TextChanged(object sender, EventArgs e)
+        {
+            string num = txtNumber2.Text;
+
+            bool isValidInt = Int32.TryParse(num, out int number2);
+            if (txtNumber2.Text == "")
+            {
+                oldValue2 = num;
+            }
+            else if ( !isValidInt)
+            {
+                txtNumber2.Text = oldValue2;
+            }
+            else
+            {
+                oldValue2 = txtNumber2.Text;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
